8850484858a980613338a37f9155dac00b2b5ab8
fuurashima <icano@outlook.cl>
2018-05-19 00:59:20 -0400

    Update el_mejor_servicio.py
    finalizado

--- el_mejor_servicio.py
+++ el_mejor_servicio.py
@@ -1,25 +1,32 @@
-def Diccionario(dicc_empleado,rut,nombre,apellido,depto,sbase):
-    dicc_empleado[rut]= rut, nombre, apellido, depto, sbase
-    return dicc_empleado
-def Diccionario2(dicc_empleado,rut,nombre,apellido,depto,sbase):
-    dicc_empleado[depto] = rut, nombre, apellido, depto, sbase
-    return dicc_empleado
-#Hola
+def insertar_empleado(diccionario_empleado,rut,nombre,apellido,depto,sbase):
+    diccionario_empleado[rut] = (nombre,apellido,depto,sbase)
+    return diccionario_empleado
+def insertar_empleado_dpto(diccionario_depto,rut,nombre,apellido,depto,sbase):
+    if depto not in diccionario_depto:
+        diccionario_depto[depto] = {}
+    diccionario_depto[depto][rut] = (nombre,apellido,depto,sbase)
+    return diccionario_depto
+def actualizar_empleado_sbase(diccionario_empleado, diccionario_depto, rut, new_sbase):
+    datos_empleado = diccionario_empleado[rut]
+    diccionario_empleado[rut] = (datos_empleado[0], datos_empleado[1], datos_empleado[2], new_sbase)
+    diccionario_depto[datos_empleado[2]][rut] = (datos_empleado[0], datos_empleado[1], datos_empleado[2], new_sbase)
+
 
 print("\n\n\t El Mejor Servicio")
 #Listas
-dicc_empleado = {}
-lista2 =[]
+diccionario_empleado = {}
+diccionario_depatamentos = {}
+lista2 = []
 
-opc=0
-while opc!=6:
+opc = 0
+while opc != 6:
     print("\n\n 1.- Ingreso de Cliente Manualmente: ")
     print("\n\n 2.- Consulta Departamentos: ")
     print("\n\n 3.- Llenar lista de Empleados automáticamente: ")
     print("\n\n 4.- Actualizar sueldo base sobre Rut: ")
     print("\n\n 5.- Llenar una lista y averiguar el promedio,mayor y menor ")
     print("\n\n 6.- Salir")
-    opc=int(input("Ingrese Opcion: "))
+    opc = int(input("Ingrese Opcion: "))
 
     if opc == 1:
         rut = input("\n\t Ingrese RUT: ")
@@ -27,70 +34,64 @@ while opc!=6:
         apellido = input("\n\t Ingrese Apellido: ")
         depto = input("\n\t Ingrese Depto: ")
         sbase = input("\n\t Ingrese Sueldo Base: ")
-        dicc_empleado = Diccionario(dicc_empleado, rut, nombre, apellido, depto, sbase)
-        dicc_empleado = Diccionario2(dicc_empleado, rut, nombre, apellido, depto, sbase)
-
-
+        diccionario_empleado = insertar_empleado(diccionario_empleado, rut, nombre, apellido, depto, sbase)
+        diccionario_depatamentos = insertar_empleado_dpto(diccionario_depatamentos,rut,nombre,apellido,depto,sbase)
 
     elif opc == 2:
-        clave = dicc_empleado.keys()
-        listadepto = list(dicc_empleado.values())
-        print("\n\n Los departamentos son: ", clave)
-
+        clave = diccionario_empleado.keys()
+        listadepto = list(diccionario_empleado.values())
+        print("\n\n Los departamentos son: ", clave, listadepto)
 
     elif opc == 3:
 
-        dicc_empleado[169106614]=(169106614, "Ivan","Cano","Informática", 280000)
-        dicc_empleado[199349775] = (199349775, "Pamela","Fritis", "Tecnóloga", 780000)
-        dicc_empleado[85486845] = (8548684-5, "Hector", "Villarroel","Informático", 56333)
-        dicc_empleado[94861616] = (9486161-6, "Dayana", "Gonzalez","Tecnologa", 10090090)
-        dicc_empleado[94869528] = (94869528, "Fernando", "Silva","Profesor", 5420000)
+        insertar_empleado(diccionario_empleado, "16910661-4", "Ivan", "Cano","Informática", 280000)
+        insertar_empleado(diccionario_empleado, "19934977-5", "Pamela", "Fritis","Tecnóloga", 780000)
+        insertar_empleado(diccionario_empleado, "8548684-5", "Hector", "Villarroel","Informática", 56333)
+        insertar_empleado(diccionario_empleado, "9486161-6", "Dayana", "Gonzalez","Tecnóloga", 10090090)
+        insertar_empleado(diccionario_empleado, "9486952-8", "Fernando", "Silva","Profesor", 5420000)
+        insertar_empleado_dpto(diccionario_depatamentos, "16910661-4", "Ivan", "Cano","Informática", 280000)
+        insertar_empleado_dpto(diccionario_depatamentos, "19934977-5", "Pamela", "Fritis", "Tecnóloga", 780000)
+        insertar_empleado_dpto(diccionario_depatamentos, "8548684-5", "Hector", "Villarroel","Informática", 56333)
+        insertar_empleado_dpto(diccionario_depatamentos, "9486161-6", "Dayana", "Gonzalez","Tecnóloga", 10090090)
+        insertar_empleado_dpto(diccionario_depatamentos, "9486952-8", "Fernando", "Silva","Profesor", 5420000)
         print("\n\n\t Datos ingresados..... ")
 
     elif opc == 4:
-        #for i in range(0, len(dicc_empleado), 1):
-        #clave = dicc_empleado.keys()
-        #listadepto = list(dicc_empleado.values())
-        #print("\n\n ",listadepto)
-        #for nombre,apellido,depto,sbase in listadepto:
-            #print(" ", nombre, apellido, depto, sbase)
 
         new_sbase = 0
         y = 0
-        print (dicc_empleado)
         while y == 0:
             rut = input("Ingresa el rut para modificar: ")
-            existe = (rut in dicc_empleado)
+            #import pdb; pdb.set_trace()
+            existe = (rut in diccionario_empleado)
             while existe == True:
-                valor = dicc_empleado.get(rut)
-                print (" ", valor)
+                valor = diccionario_empleado[rut]
+                print(" ", valor)
                 new_sbase = int(input("\n\n Ingrese nuevo sueldo: "))
-                dicc_empleado[rut] = (valor[0], valor[1], valor[2], valor[3],valor[4], new_sbase)
-                print ("\n\n Nueva lista de empleados actualizada: ")
-                print (dicc_empleado)
+                actualizar_empleado_sbase(diccionario_empleado, diccionario_depatamentos, rut, new_sbase)
                 y = 1
                 print("\n\n Sueldo Actualizado")
-        
+                sum_sbase = 0
+                for key, value in diccionario_empleado.items():
+                    print(key, value)
+                    sum_sbase += value[3]
+                print("Sueldo Base: " + str(sum_sbase))
+
+                existe = False
 
     elif opc == 5:
         print("\n\n Bienvenidos al ejercicio 2")
-        prom =0.0
+        prom = 0.0
         num = 0.0
         x = 0
         x = int(input("\n\n Ingrese cantidad de números que va a ingresar: "))
-        for i in range (x):
-            num = float(input("\n\n Ingrese los números a ingresar: "))
-            lista2.append (num)
+        for i in range(x):
+            num = float(input("\n\n Ingrese los números"))
+            lista2.append(num)
             prom = prom + num
-        print ("Lista: ",lista2)
-        print ("El Promedio es: ", prom/len(lista2))
+        print("Lista: ", lista2)
+        print("El Promedio es: ", prom / len(lista2))
         min(lista2)
-        print ("El valor minimo es: ", min(lista2))
+        print("El valor minimo es: ", min(lista2))
         max(lista2)
-        print ("El Valor Mayor es: ", max(lista2))
-
-
-
-
-
-
+        print("El Valor Mayor es: ", max(lista2))
